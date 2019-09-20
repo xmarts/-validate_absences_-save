5ffaa3ea1ab4b9ee0ae1f7c535df4a74f39a8a3d
soto <victor.alonso@xmarts.com>
2019-09-19 21:29:03 -0500

    actualizacion

---  validate_absences_ save/models/models.py
+++  validate_absences_ save/models/models.py
@@ -13,14 +13,16 @@ class primermodulo(models.Model):
     @api.model
     def create(self, values):
         val= super(primermodulo, self).create(values)
-        fecha_actual = val.fecha_ac
-        fecha_ex = val.holiday_status_id.validity_stop
+        fecha_actual = val.fecha_ac     
         print(fecha_actual)
-        print(fecha_ex)
-        busque = self.env['hr.leave.allocation'].search([('employee_id','=',val.employee_id.id),('extended_permission','=',True),('holiday_status_id','=',val.holiday_status_id.id)],limit=1)
+        busque = self.env['hr.leave.allocation'].search([('employee_id','=',val.employee_id.id),('holiday_status_id','=',val.holiday_status_id.id)],limit=1)
         em = busque.extended_permission
+        fecha_ex = busque.vencimiento
+        print(fecha_ex)
         if em == False:
-            if fecha_actual and fecha_ex:
+            if fecha_ex:
                 if  fecha_actual > fecha_ex:
-                    raise ValidationError('Estos días ya vencieron, selecciona otra asignación distinta a esta e intentalo de nuevo.')
+                    raise ValidationError('Estos días ya vencieron, selecciona otra asignación distinta a esta e intentalo de nuevo.')          
+            else:
+                  raise ValidationError('el tipo de Asignacion no contine una fecha de vencimiento')
         return val
